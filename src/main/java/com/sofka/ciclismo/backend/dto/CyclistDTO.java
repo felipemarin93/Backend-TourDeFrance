a6c1b1f68d426b63dc603dbb2555dcf64a5c6314
felipemarin93 <fmarin93@misena.edu.co>
2022-08-25 19:16:08 -0500

    Se agregan DTOs

---
+++ src/main/java/com/sofka/ciclismo/backend/dto/CyclistDTO.java
@@ -0,0 +1,30 @@
+package com.sofka.ciclismo.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class CyclistDTO {
+
+    private String idCyclist;
+
+    @NotBlank
+    private String fullName;
+
+    @NotBlank
+    @Indexed(unique = true)
+    @Size(min = 1, max =3)
+    private String cyclistNumber;
+
+    @NotBlank
+    private String teamName;
+    @NotBlank
+    private String nationality;
+}
---
+++ src/main/java/com/sofka/ciclismo/backend/dto/TeamDTO.java
@@ -0,0 +1,48 @@
+package com.sofka.ciclismo.backend.dto;
+
+import com.sofka.ciclismo.backend.collection.Cyclist;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TeamDTO {
+
+    private String teamId;
+
+    @NotBlank
+    private String teamName;
+
+    @NotBlank
+    @Indexed(unique = true)
+    @Size(min = 1, max =3)
+    private String teamCode;
+
+    @NotBlank
+    private String country;
+
+    private Set<Cyclist> cyclistTeam;
+
+    public TeamDTO(String teamId, String teamName, String teamCode, String country){
+        this.teamId = teamId;
+        this.teamName = teamName;
+        this.teamCode = teamCode;
+        this.country = country;
+
+    }
+
+    public Set<Cyclist> getCyclistTeam(){
+        this.cyclistTeam  = Optional.ofNullable(cyclistTeam).orElse(new HashSet<>());
+        return cyclistTeam;
+    }
+
+}
