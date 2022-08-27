2f7fa23f1386a59025e7c1e682ef96d8488bd801
felipemarin93 <fmarin93@misena.edu.co>
2022-08-26 20:10:23 -0500

    Agregando 3 Casos de uso mas al proyecto

---
+++ src/main/java/com/sofka/ciclismo/backend/usecase/ListCyclistsByNationalityUseCase.java
@@ -0,0 +1,24 @@
+package com.sofka.ciclismo.backend.usecase;
+
+import com.sofka.ciclismo.backend.dto.CyclistDTO;
+import com.sofka.ciclismo.backend.mapper.CyclistMapper;
+import com.sofka.ciclismo.backend.repository.CyclistRepository;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+public class ListCyclistsByNationalityUseCase implements Function<String, Flux<CyclistDTO>> {
+    private final CyclistRepository cyclistRepository;
+    private final CyclistMapper cyclistMapper;
+
+    public ListCyclistsByNationalityUseCase(CyclistRepository cyclistRepository, CyclistMapper cyclistMapper){
+        this.cyclistRepository = cyclistRepository;
+        this.cyclistMapper = cyclistMapper;
+    }
+
+    @Override
+    public Flux<CyclistDTO> apply(String nationality){
+        return cyclistRepository.findCyclistsByNationality(nationality)
+                .map(cyclistMapper.mapCyclistToCyclistDTO());
+    }
+}
